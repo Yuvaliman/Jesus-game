eb5ea354958b4f22b162f19662517aa5771177b6
Yuvaliman <117382496+Yuvaliman@users.noreply.github.com>
2023-12-17 21:47:21 +0200

    player movement and animation

--- Jesus game/Assets/Scripts/Movement.cs
+++ Jesus game/Assets/Scripts/Movement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D body;
+    public Animator anim;
+
+    private float horizontal;
+    private float vertical;
+
+    public float MoveSpeed = 5.0f;
+
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (horizontal != 0 || vertical != 0)
+        {
+            anim.SetBool("IsWalking", true);
+            if (Input.GetKey(KeyCode.LeftShift)) 
+            {
+                anim.SetBool("IsRunning", true);
+            }
+            else
+            {
+                anim.SetBool("IsRunning", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+        body.velocity = new Vector2(horizontal * MoveSpeed, vertical * MoveSpeed);
     }
 }
