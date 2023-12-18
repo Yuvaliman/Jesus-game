f0bf72e170a55ffb7278896fd23a5083f9fe4c45
Yuvaliman <117382496+Yuvaliman@users.noreply.github.com>
2023-12-18 21:02:46 +0200

    Added orb
    need to accelerate the speed of the orb after a certain distance

---
+++ Jesus game/Assets/Scripts/OrbFloat.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbFloat : MonoBehaviour
+{
+    public Transform playerTransform;
+    public SpriteRenderer PlayerSpriteRenderer;
+    public Transform orbTransform;
+    public float horizontalMovementSpeed = 5.0f; // Adjust the horizontal speed as needed
+    public float verticalMovementSpeed = 2.0f; // Adjust the vertical speed as needed
+    public float lerpFactor = 0.1f; // Adjust the interpolation factor
+    public Vector3 offset = new Vector3(-0.45f, 0.25f, 0.0f); // Set the desired offset
+    public float closeDistanceThreshold = 1.5f; // Set the threshold for close distance
+    public float amplitude = 0.01f; // Adjust the amplitude for vertical movement
+
+    void Update()
+    {
+        if (playerTransform != null && orbTransform != null)
+        {
+            float flipMultiplier = PlayerSpriteRenderer.flipX ? -1f : 1f;
+            Vector3 targetPosition = playerTransform.position + new Vector3(offset.x * flipMultiplier, offset.y, offset.z);
+            Vector3 newPosition = Vector3.Lerp(orbTransform.position, targetPosition, lerpFactor * Time.deltaTime * horizontalMovementSpeed);
+
+            float distanceToPlayer = Vector3.Distance(orbTransform.position, playerTransform.position);
+            bool isClose = distanceToPlayer <= closeDistanceThreshold;
+
+            if (isClose)
+            {
+                float verticalOffset = Mathf.Sin(Time.time * verticalMovementSpeed) * amplitude; // Adjust amplitude as needed
+                newPosition.y += verticalOffset;
+            }
+
+            orbTransform.position = newPosition;
+        }
+    }
+}
