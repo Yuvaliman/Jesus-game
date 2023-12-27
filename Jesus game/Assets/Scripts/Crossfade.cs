8e7c9aa4674fb620109055f2ebb7cad50a27dfe8
Guy1414 <0606guy@gmail.com>
2023-12-27 18:46:40 +0200

    Scene transitions better

--- Jesus game/Assets/Scripts/Crossfade.cs
+++ Jesus game/Assets/Scripts/Crossfade.cs
@@ -7,6 +7,7 @@ public class Crossfade : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
+    public GameObject image;
 
     public void CrossfadeAnimation(int scene)
     {
