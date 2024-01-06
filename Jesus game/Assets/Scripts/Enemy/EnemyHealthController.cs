75500126ba8c62efd6b88eb12823122074f91ffb
Guy1414 <0606guy@gmail.com>
2024-01-06 12:40:55 +0200

    Player Damages Enemies!

--- Jesus game/Assets/Scripts/Enemy/EnemyHealthController.cs
+++ Jesus game/Assets/Scripts/Enemy/EnemyHealthController.cs
@@ -1,37 +1,25 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 
 public class EnemyHealthController : MonoBehaviour
 {
     public Rigidbody2D body;
 
-    [SerializeField]
-    private float _currentHealth;
+    [SerializeField] private float _currentHealth;
+    [SerializeField] private float _maximumHealth;
 
-    [SerializeField]
-    private float _maximumHealth;
-
-    bool isDeadAnimationStarted = false;
-    float deathDelay = 1.0f;
+    private bool isDeadAnimationStarted = false;
 
     public float RemainingHealthPercentage
     {
-        get
-        {
-            return _currentHealth / _maximumHealth;
-        }
+        get { return _currentHealth / _maximumHealth; }
     }
 
     public bool IsInvincible { get; set; }
 
     public UnityEvent OnDied;
-
     public UnityEvent OnDamaged;
-
     public UnityEvent OnHealthChanged;
 
     public void TakeDamage(float damageAmount)
@@ -45,15 +33,11 @@ public class EnemyHealthController : MonoBehaviour
 
         OnHealthChanged.Invoke();
 
-        if (_currentHealth < 0)
+        if (_currentHealth <= 0)
         {
             _currentHealth = 0;
-        }
-
-        if (_currentHealth == 0)
-        {
             OnDied.Invoke();
-            body.velocity = new Vector2(0, 0);
+            body.velocity = Vector2.zero;
             StartCoroutine(DeathRoutine());
         }
         else
@@ -79,18 +63,23 @@ public class EnemyHealthController : MonoBehaviour
         }
     }
 
-    // start death animation
-
+    // Start death animation and destroy the entire prefab
     IEnumerator DeathRoutine()
     {
         if (!isDeadAnimationStarted)
         {
             isDeadAnimationStarted = true;
+
+            // Stop the enemy's movement by setting its velocity to zero
+            body.velocity = Vector2.zero;
+
+            // No delay, destroy immediately
+            Destroy(transform.root.gameObject);
         }
 
-        yield return new WaitForSeconds(deathDelay);
-
-
         isDeadAnimationStarted = false;
+
+        // Yield break to end the coroutine
+        yield break;
     }
 }
