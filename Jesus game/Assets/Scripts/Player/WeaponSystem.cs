9643577ecd8d1a015957a9fce90cbf6a21e19112
Guy1414 <0606guy@gmail.com>
2025-05-29 19:38:47 +0300

    Made player weapons point to mouse

--- Jesus game/Assets/Scripts/Player/WeaponSystem.cs
+++ Jesus game/Assets/Scripts/Player/WeaponSystem.cs
@@ -4,16 +4,25 @@ public class WeaponSystem : MonoBehaviour
 {
     public GameObject Sword;
     public GameObject Bow;
+    public Transform WeaponHolder; // The transform that holds the weapon (positioned at character's hand/body)
 
-    // Start is called before the first frame update
+    Camera mainCam;
+
     void Start()
     {
+        mainCam = Camera.main;
+
         Sword.SetActive(true);
         Bow.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        HandleWeaponSwitch();
+        RotateWeaponToMouse();
+    }
+
+    void HandleWeaponSwitch()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -25,5 +34,16 @@ public class WeaponSystem : MonoBehaviour
             Sword.SetActive(false);
             Bow.SetActive(true);
         }
+    }
+
+    void RotateWeaponToMouse()
+    {
+        Vector3 mouseWorldPosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+
+        Vector3 direction = mouseWorldPosition - WeaponHolder.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        WeaponHolder.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
